c02a1c22c1965650f35a5c22e838a354266b3c1d
harshithg24 <harshith.gbdvt@gmail.com>
2023-11-27 04:07:45 +0530

    Creating table

---
+++ Nov24.sql
@@ -0,0 +1,8 @@
+USE harshith;
+
+CREATE TABLE mobile(id int,brand varchar(20),model varchar(10),price bigint,
+created_at timestamp,created_by varchar(20),modified_at timestamp,modified_by varchar(20));
+
+DESC mobile;
+
+SELECT * FROM mobile;
